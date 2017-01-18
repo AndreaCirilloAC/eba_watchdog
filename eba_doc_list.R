f8a1dc49e344f35dbe0151337ef781e12c039fef
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-01-18 01:24:17 +0100

    fixed semantic bug on names object definition git push

--- eba_doc_list.R
+++ eba_doc_list.R
@@ -1,7 +1,7 @@
 library(rio)
 library(rvest)
 library(dplyr)
-acc_and_aud_code <- read_html("Accounting and auditing - European Banking Authority.html")
+acc_and_aud_code <- read_html("https://www.eba.europa.eu/regulation-and-policy/accounting-and-auditing")
 
 acc_and_aud_code %>% 
   html_nodes(" #activities .Title") %>% 
@@ -19,14 +19,14 @@ View(acc_and_aud_db)
 
 # credit risk regulation and guidelines
 
-credit_risk <- read_html('Regulatory technical standards on disclosure of information related to the countercyclical capital buffer - European Banking Authority.html')
+credit_risk <- read_html('https://www.eba.europa.eu/regulation-and-policy/credit-risk/-/activity-list/tSWPlf98sl2V/more')
 credit_risk %>% 
   html_nodes(" #activities .Title") %>% 
   html_attr("href") -> link
 
 credit_risk %>% 
   html_nodes("#activities .Title") %>% 
-  html_text() %>% names
+  html_text() -> names
 
 credit_risk_db <- data.frame(names,link)
 
@@ -53,3 +53,4 @@ info_extractor <-  function(urls){
   return(data.frame(status, release_date))
 }
 
+info_extractor(as.character(credit_risk_db[1,2]))
